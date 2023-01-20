b198da85782e5bcd571e318067300dd7917c3b39
Genzo4 <genzo@bk.ru>
2023-01-20 10:51:31 +1000

    do args

--- main.py
+++ main.py
@@ -1,6 +1,61 @@
+import argparse
+
 from ya_music_rnd import YandexMusicRnd
 
 
+version = '1.0.0.3'
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(
+        prog='ya_music_rnd',
+        description='Yandex Music Rnd v.%s' % version + ' (c) 2023 Genzo',
+        add_help=False
+        )
+
+    parser.add_argument(
+        '-max',
+        '--max_index',
+        type=int,
+        default=10000000,
+        metavar='max_artist_index',
+        help='Максимальный индекс для поиска артиста')
+
+    parser.add_argument(
+        '-no',
+        '--dont_open_in_browser',
+        action='store_true',
+        help='Не открывать найденный результат в браузере, но вывести его на экран'
+    )
+
+    parser.add_argument(
+        '-v',
+        '--version',
+        action='version',
+        help='Показать версию',
+        version='%(prog)s v.{}'.format(version)
+    )
+
+    parser.add_argument(
+        '-h', '-?',
+        '--help',
+        action='help',
+        help='Показать помощь и выйти из программы'
+    )
+
+    args = parser.parse_args()
+    return args
+
+
+def main():
+    args = parse_args()
+
+    ya_rnd = YandexMusicRnd(max_int=args.max_index, open_url=not(args.dont_open_in_browser))
+    site = ya_rnd.get_artist()
+
+    if args.dont_open_in_browser:
+        print(site)
+
+
 if __name__ == '__main__':
-    ya_rnd = YandexMusicRnd()
-    ya_rnd.get_artist()
+    main()
